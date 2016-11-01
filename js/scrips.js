3dee463977dd7ec80ec50745bc35c935b08697f6
Rodolfo <rodikuman@gmail.com>
2016-11-01 11:13:49 -0500

    template beta

---
+++ js/scrips.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$('button').on('click',function(){
+		$(this).closest('#promo button').html('<i class="fa fa-phone" aria-hidden="true"></i><span> 01 (800) 000 1111</span>');
+		
+	});	
+	
+});
